f339ed1781427a93141b103524ca03f080612714
Murad <nuradhussen082@gmail.com>
2025-04-10 09:43:04 +0300

    minor change

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -28,7 +28,7 @@ export const Home = () => {
 
             <a
               onClick={handleOpenPdf}
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+              className="cursor-pointer border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
              hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
             >
               Download Resume
