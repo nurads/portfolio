3bc3fe4b8cbd12068bbc803de020aba59c3809a8
Murad <nuradhussen082@gmail.com>
2025-06-01 23:35:02 +0300

    backup

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -29,6 +29,18 @@ export const About = () => {
     "BeautifulSoup",
   ];
 
+  const devopsSkills = [
+    "Azure",
+    "AWS",
+    "Git",
+    "Github Actions",
+    "CI/CD",
+    "Jenkins",
+    "Lambda",
+    "Ansible",
+    "Docker",
+  ];
+
   const workExperiences = [
     {
       title: "Senior Software Engineer",
@@ -136,6 +148,21 @@ export const About = () => {
                     </span>
                   ))}
                 </div>
+              </div>
+            </div>
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">DevOps</h3>
+              <div className="flex flex-wrap gap-2">
+                {devopsSkills.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
               </div>
             </div>
           </div>
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -12,33 +12,47 @@ export const Projects = () => {
             {" "}
             Featured Projects
           </h2>
-          <div className="grid grid-cols-1 gap-6">
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
-              <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
-                    "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
 
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
-                >
-                  View Project →
-                </a>
+          <div className="grid grid-cols-1 gap-6">
+            <div className="flex justify-between p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+              <img
+                class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-72 md:rounded-none md:rounded-s-lg"
+                src="https://imgv3.fotor.com/images/blog-richtext-image/a-shadow-of-a-boy-carrying-the-camera-with-red-sky-behind.jpg"
+                alt=""
+              ></img>
+              <div className="p-4 flex-col flex justify-around ">
+                <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+                <p className="text-gray-400 mb-4">
+                  Scalable cloud infrastructure management with real-time
+                  monitoring and automated scaling.
+                </p>
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="#"
+                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  >
+                    View Project →
+                  </a>
+                  <a
+                    href="#"
+                    className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  >
+                    Source Code
+                  </a>
+                </div>
               </div>
             </div>
           </div>
